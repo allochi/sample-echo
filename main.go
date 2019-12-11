e20c935e9fa51ec5c642f92a83f4b966c2288f68
allochi <allochi@gmail.com>
2019-12-11 13:41:54 +0100

    add some comments

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ func main() {
 	router := echo.New()
 	router.GET("/", handlers.Home)
 
+	// add `data` section to API
+	// -------------------------
 	data := router.Group("/data")
 	data.GET("/quotes", handlers.GetQuotes)
 	data.GET("/tenders", handlers.GetTenders)
 
+	// add `secrets` section to API with authentication
+	// ------------------------------------------------
 	secrets := router.Group("/secrets")
 	secrets.Use(middleware.BasicAuth(handlers.Authenticate))
 	secrets.GET("/names", handlers.GetNames)
